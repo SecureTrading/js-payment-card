452718652cd12b3be83a5220e05cb06916bc4949
James Simpson <james.simpson@securetrading.com>
2018-08-28 14:14:48 +0000

    Integrationtests (#8)
    * Update integration tests to use Jest
    
    * Updated to use more of the Jest framework to run the tests
    
    * Don't run integration tests as part of npm test run - there is a separate npm integration which is run during the travis run
    
    * Update timeout to allow longer for browserstack to return
    
    * Updated expected expiry
    
    * Added back in IE9 and 10 tests after changes made to ensure this works in those browsers

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {"testEnvironment": "node",
+		  "moduleNameMapper": {
+		      "\\.(css|less|png)$": "<rootDir>/test/__mocks__/fileMock.js",
+		      "\\.(html)$": "<rootDir>/test/__mocks__/htmlMock.js"
+		  }
+		  
+		 }
