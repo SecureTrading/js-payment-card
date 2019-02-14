5107f20e5324abdace8096a3bf9c3aed49ace798
james-simpson-ST <james.simpson@securetrading.com>
2019-02-14 00:57:23 +0000

    Include @bable/polyfill

--- src/payment-card.js
+++ src/payment-card.js
@@ -1,4 +1,3 @@
-import "@babel/polyfill";
 import styles from "./styles.css"; // eslint-disable-line no-unused-vars
 import template from "./template.html";
 import chipLogo from "./images/chip.png";
